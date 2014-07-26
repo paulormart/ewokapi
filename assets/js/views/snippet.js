0b38a1a39995b7f7fe0a1c326462f0a4075687aa
paulo <paulo.rmartins@gmail.com>
2014-07-26 12:30:23 +0100

    preparing project for backbone

--- assets/js/views/snippet.js
+++ assets/js/views/snippet.js
@@ -6,6 +6,21 @@ app.SnippetView = Backbone.View.extend({
     className: 'snippetContainer',
     template: _.template($('#snippetTemplate').html()), // this is the snippetTemplate id from the script tag, which willl be replaced
 
+
+    // Events
+    events: {
+        'click .delete':'deleteSnippet'
+    },
+
+    deleteSnippet: function(){
+
+        // Delete model
+        this.model.destroy();
+
+        //Delete view
+        this.remove();
+    },
+
     render: function(){
 
         this.$el.html(this.template(this.model.attributes)); // this.$el is what we have just defined in the tagName above
--- assets/js/views/snippets.js
+++ assets/js/views/snippets.js
@@ -2,6 +2,7 @@
 var app = app || {};
 
 app.SnippetsView = Backbone.View.extend({
+
     el: '#snippets',
 
     initialize: function(initialSnippets){
@@ -9,32 +10,16 @@ app.SnippetsView = Backbone.View.extend({
         this.render();
 
         // Initialize the event listener
-        this.listenTo(this.collection, 'add', this.renderSnippet());
+        this.listenTo(this.collection, 'add', this.renderSnippet );
 
-
-    },
-
-    // render snippets by render each snippet in its collection
-    render: function(){
-        this.collection.each(function(item){
-            this.renderSnippet(item);
-        }, this);
-    },
-
-    // render a snippet by creating a snippetView and appending the element it renders to the snippet's element
-    renderSnippet: function(item){
-        var snippetView = new app.SnippetView({
-            model: item
-        });
-        this.$el.append(snippetView.render().el);
     },
 
     // Events
-
-    events:{
+    events: {
         'click #add':'addSnippet'
     },
 
+    // Functions
     addSnippet: function( e ) {
 
         e.preventDefault();
@@ -49,6 +34,27 @@ app.SnippetsView = Backbone.View.extend({
         });
 
         this.collection.add( new app.Snippet( formData ) );
+    },
+
+
+
+    // render snippets by render each snippet in its collection
+    render: function(){
+        this.collection.each(function(item){
+            this.renderSnippet(item);
+        }, this);
+    },
+
+    // render a snippet by creating a snippetView and appending the element it renders to the snippet's element
+    renderSnippet: function(item){
+        var snippetView = new app.SnippetView({
+            model: item
+        });
+        this.$el.append(snippetView.render().el);
     }
+
+
+
+
 
 });
